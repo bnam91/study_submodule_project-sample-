7b0837fe6f7594582ba7beffdeeeb9599c1935ed
bnam <darling_91@naver.com>
2025-12-28 04:34:59 +0900

    submodule-study

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-const { hello } = require("module-test");
+// 첫 번째 서브모듈 (hello)
+// 두 번째 서브모듈 (add, multiply)
+const { hello } = require("study-submodule");
+const { add, multiply } = require("study-submodule-02");
 
-console.log(hello("Docker"));
+console.log(hello("Docker & Submodule Test"));
+
+const a = 3;
+const b = 4;
+
+console.log("add:", add(a, b));
+console.log("multiply:", multiply(a, b));
